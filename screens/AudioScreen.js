13756d7d3e88583e210abab6019aaa96c7cad289
Mpho Chuene <123539842+mphoChuene@users.noreply.github.com>
2023-11-01 12:49:15 +0200

    Firestore integration complete

--- screens/AudioScreen.js
+++ screens/AudioScreen.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { app, db } from "../firebaseConfig";
 import { Audio } from "expo-av";
 
-export default function App() {
-  const [recording, setRecording] = React.useState();
-  const [recordings, setRecordings] = React.useState([]);
+export default function AudioScreen() {
+  const [recording, setRecording] = useState();
+  const [recordings, setRecordings] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSound, setCurrentSound] = useState(null);
 
   async function startRecording() {
+    if (currentSound) {
+      await currentSound.stopAsync();
+    }
+
     try {
       const perm = await Audio.requestPermissionsAsync();
       if (perm.status === "granted") {
@@ -19,7 +33,9 @@ export default function App() {
         );
         setRecording(recording);
       }
-    } catch (err) {}
+    } catch (err) {
+      // Handle errors
+    }
   }
 
   async function stopRecording() {
@@ -28,10 +44,18 @@ export default function App() {
     await recording.stopAndUnloadAsync();
     let allRecordings = [...recordings];
     const { sound, status } = await recording.createNewLoadedSoundAsync();
+
+    // Upload the recording to Firebase
+    const docRef = await addDoc(collection(db, "recordings"), {
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI(),
+    });
+
     allRecordings.push({
       sound: sound,
       duration: getDurationFormatted(status.durationMillis),
       file: recording.getURI(),
+      firebaseDocId: docRef.id,
     });
 
     setRecordings(allRecordings);
@@ -45,6 +69,32 @@ export default function App() {
       : `${Math.floor(minutes)}:${seconds}`;
   }
 
+  async function togglePlayback(sound) {
+    if (isPlaying && currentSound === sound) {
+      await sound.pauseAsync();
+    } else {
+      if (currentSound && currentSound !== sound) {
+        await currentSound.stopAsync();
+      }
+      await sound.playAsync();
+      setCurrentSound(sound);
+    }
+    setIsPlaying(!isPlaying);
+  }
+
+  async function clearRecordings() {
+    for (const recordingLine of recordings) {
+      await deleteDoc(doc(db, "recordings", recordingLine.firebaseDocId));
+    }
+
+    if (currentSound) {
+      currentSound.stopAsync();
+      setCurrentSound(null);
+    }
+    setIsPlaying(false);
+    setRecordings([]);
+  }
+
   function getRecordingLines() {
     return recordings.map((recordingLine, index) => {
       return (
@@ -53,16 +103,15 @@ export default function App() {
             Recording #{index + 1} | {recordingLine.duration}
           </Text>
           <Button
-            onPress={() => recordingLine.sound.replayAsync()}
-            title="Play"
-          ></Button>
+            onPress={() => togglePlayback(recordingLine.sound)}
+            title={
+              isPlaying && currentSound === recordingLine.sound
+                ? "Pause"
+                : "Play"
+            }></Button>
         </View>
       );
     });
-  }
-
-  function clearRecordings() {
-    setRecordings([]);
   }
 
   return (
